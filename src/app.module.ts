03a70a50ca8e7c9c4035e0622dc912d4af13679e
kidp2h <thjnhsoajca@gmail.com>
2022-08-22 18:19:10 +0700

    #15 - setup graphql with apollo server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_URL),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.graphql'),
+      playground: true,
+      csrfPrevention: false,
+      debug: true,
+      path: '/',
+      context: ({ req, res }) => ({ req, res }),
+    }),
   ],
   providers: [],
 })
